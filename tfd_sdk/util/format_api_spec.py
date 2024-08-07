25f2e78b86d3e2b168c50caf6e7e79b1e236e8b0
Erik Karlsson <erikdkarlsson8@gmail.com>
2024-08-07 09:31:09 -0700

    Refactored some code. Started using glob and yaml to parse and update the api spec.

--- tfd_sdk/util/format_api_spec.py
+++ tfd_sdk/util/format_api_spec.py
@@ -1,37 +1,46 @@
 import pathlib
+from pathlib import Path
+from typing import Dict
+
 import yaml
 import json
 
 PROJ_DIR = pathlib.Path(".").resolve()
 
 
-def get_specs(spec_glob="*.openapi.yml"):
-    # return dict([(p.name.split(".")[0], p) for p in PROJ_DIR.glob("./openapi/*.openapi.yml")])
-    return dict([(p.name.split(".")[0], p) for p in PROJ_DIR.glob(f"./openapi/{spec_glob}")])
+def get_specs(spec_glob="*.openapi.yml") -> dict[str, Path]:
+    return dict(
+        [(p.name.split(".")[0], p) for p in PROJ_DIR.glob(f"./openapi/{spec_glob}")]
+    )
 
 
-def get_spec_defaults() -> dict:
-    return get_specs(spec_glob="*.defaults.yml")
+def get_spec_defaults() -> dict[str, Path]:
+    return {"defaults": get_specs(spec_glob="*.defaults.yml").pop("openapi")}
+    # return get_specs(spec_glob="*.defaults.yml")
 
 
 def to_json(yml_obj: dict) -> dict:
     return json.loads(json.dumps(yml_obj))
 
 
+def print_specs(specs: dict) -> None:
+    print("Specs:")
+    for k, v in specs.items():
+        print(f"\t{k}: {v}")
+
+
 def main():
     specs = get_specs()
     defaults = get_spec_defaults()
 
-    print(defaults)
+    print_specs(specs)
+    print_specs(defaults)
 
-    with open(specs['meta'], "rb") as yml_file:
+    with open(defaults["defaults"], "rb") as defaults_file:
+        defaults_dict: dict = yaml.safe_load(defaults_file)
+
+    with open(specs["meta"], "rb") as yml_file:
         yml_obj: dict = yaml.safe_load(yml_file)
-        tags = json_obj['tags']
-        print(yml_obj.keys())
-
-        print(json_obj.keys())
-        print(tags)
-        # print(yaml.dump(yml_obj))
 
 
 if __name__ == "__main__":
